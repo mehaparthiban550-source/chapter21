eefdd211234532581093aa8a0abb5ba836c21098
mehaparthiban550-source <mehaparthiban550@gmail.com>
2026-07-18 07:14:01 +0530

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,125 @@
+const characters = [
+
+{
+    name: "V",
+    image: "images/v2.jpg",
+    message: `Saengil chukah hanmida .Oh Sorry eventhough i am korean i would like to say it in Tamil the mother of all languages .Iniya Piranthanal nalvalthukkal.Sorry dont mind my pronunciation. Here cut your cake. `
+},
+
+{
+    name: "Jungkook",
+    image: "images/jung.jpg",
+    message: `Taehyung hyung forgot to bring the baloons ,anyway i bought it for you.I know he is your bias but still Meha sent me to wish you.Saenghil chuka haminda,also remember no matter how many people discourage you, please dont give up because i am also the kind of person who would never giveup .`
+},
+
+{
+    name: "Jennie",
+    image: "images/jennie.jpg",
+    message: `Annyeong Jennie Unnie here .Oh i have to tell you look as stylish as me, you look gorgeous in whatever you wear, dont listen to the haters`
+},
+
+{
+    name: "Lisa",
+    image: "images/lisa.jpg",
+    message: `Happy Birthday khub .Hope today is as iconic as my "BlackPink in your Area" May you have a year full of confidence and wins`
+},
+
+{
+    name: "Rosé",
+    image: "images/rosee.jpg",
+    message: `Hey Birthday girl let me sing a song for you, "APT APT APT" . SING LOUDLY ,Laugh a lot you deserve the happiest birthday. Love you so much .Happy Birthday🌹 `
+},
+
+{
+    name: "Jisoo",
+    image: "images/jisoo.jpg",
+    message: `Oh did you watch Snowdrop who did you like the most, me or jung hae in oppa? Yah gwenchana i know you like this Turtle Rabbit kim more.Stay healthy ,stay happy, and keep smiling that is your mission this year.And remember SOOYAA!!  `
+},
+
+{
+    name: "Han Seojun",
+    image: "images/-Hwang-In-Yeop-U.jpg",
+    message: `Annyeong oh sorry i have a letter in my hand ,no its not for you ,Ok ok its for you.Eat something good today ,okay?And dont skip meals.Another year older but somehow you still look annoyingly pretty.Guess thats your talent.If anyone gives you a hard time today, tell me.Have fun ,laugh and dont stay up all night.I know you wont listen ,but i had to say.Happy Birthday Mirudula🤍  `
+},
+
+{
+    name: "cha eunwoo",
+    image: "images/cha eun woo.jpg",
+    message: `I know you like that Han seojun more than me but still have a birthday thats as warm as the ramen you like.Remember"Beauty Comes from inside" that is what lee suho and han seojun both stand for.And you are beautiful both inside and outside Happy Birthday💕 `
+},
+
+{
+    name: "Gojo Satoru",
+    image: "images/gojo.jpg",
+    message: `Dont look so surprised . you really thought i would forget your birthday ? i am offended😏 .You know... you're kind of unfair.Looking that good on your Birthday should be illegal.You are getting all these birthday messages today , huh?Cute.just dont forget which one you'll keep rereading.Today just look at me , you have no choice you are already under my Domain expansion.Kawaii.Happy Birthday Mirudula chan 🩵💙`
+},
+
+{
+    name: "Damon Salvatore",
+    image: "images/damon.jpg",
+    message: `Happy Birthday ,gorgeous. You look dangerously good today . Then again ... You usually do . i just have an excuse to stare at you a little longer.Blow out your candles and make a wish . if it doesnt come true, I'll just steal it for you.Happy Birthday Mirudula, the kind of beauty i would go to war for `
+},
+
+{
+    name: "Klaus Mikaelson",
+    image: "images/klaus.jpg",
+    message: `A thousand years have taught me many things ...but somehow ,you still manage to surprise me .Happy birthday mirudula. birthday are curious things. They remind us that time moves forward. for you ,i hope it brings everything you've ever desired. If anyone dares ruin yur smile today they'll discover why my name has survived for a thousand years. V was your first wish.I intend to be your last however long it takes.....`
+},
+
+{
+    name: "Stefan Salvatore",
+    image: "images/stefan.jpg",
+    message: `Happy Birthday 💖. I hope today reminds how loved you are...even if you dont always see it yourself`
+},
+
+{
+    name: "Muzan",
+    image: "images/muzan.webp",
+    message: `Another year has passed ... And somehow you've managed to keep my attention.Thats a rare accomplishment.Most people bore me you dont. Enjoy today .You have no idea how dangerous it is.. looking that beautiful.Happy Birthday.`
+},
+
+{
+    name: "Lee Min Ho",
+    image: "images/lmh.jpg",
+    message: `Happy birthday Mirudulashi . You know ... birthdays are funny. Everyone else is giving you gifts , but i think the people around you are the lucky ones for getting to spend today with you`
+},
+
+{
+    name: "Keonho",
+    image: "images/cortis-keonho-420x524.jpeg",
+    message: `NUNA NEOMU YEPPEO. I practiced this in my head so many times but somehow it is still embarassing to say . Happy Birthday Sincerely!. You know when you smile,everyone around you smiles too.So dont stop smiling today.I wanted to get you the perfect gift .. but i dont think anything is as perfect as the smile you already have.💛 `
+},
+{
+    name: "che zheyuan",
+    image: "images/che zhe.jpg",
+    message: `Happy Birthday .🩷I hope you eat your favorite food , laugh until your cheeks hurt , and forget about every little worry-even if its just for one day`
+},
+{
+    name: "Edward",
+    image: "images/edward.jpg",
+    message: `Happy birthday 💙🤍 .I know you dont like being the center of attention .. but today , you dont really have a choice.If weather happens to be unusually sunny... Maybe dont ask where I disappeared to.Here's hoping this year brings you more peaceful days than rainy ones,more meadows moments than difficulties.Come here... Let me have one dance.`
+},
+{
+    name: "J hope",
+    image: "images/hope.jpg",
+    message: ` I AM YOUR HOPE , YOU ARE MY HOPE I AM JHOPE.Today,I hope your smile is even brighter than Hope World. NO worries , no stress-just good food,good music, and lots of laughter . Dance a little , sing a little and if "MIc Drop" starts playing .. you already know what to do.💜☀️🌄`
+},
+{
+    name: "Jinu",
+    image: "images/jinu.png",
+    message: `Happy Birthday . Birthdays are one of the few things even demons cant still from you .So today no hunting .No hiding .No pretending everythings fine.For one day ,forget the demons .Forget the missions,Your fans can wait🖤`
+},
+{
+    name: "Mingyu",
+    image: "images/mingyu.jpg",
+    message: `Happy Birthday💎. you know i was going to bake a cake myself.then I remembered I'd probably spend more time decorating it than actually giving it to you.Looking gorgeous isn't something new for you . Saengil chuka haminda`
+},
+{   name: "Sukuna",
+    image: "images/sukuna.jpg",
+    message: `Happy Birthday MIrudula.This world is filled of pathetic beings... but you are the only exception .if someine dares to ruin your day ..Point them out, i will handle the rest
+    Relax . I'm joking
+    Maybe.
+    Enjoy your cake .Make your Wish. You have already caught the King of Curses' attention . Dont make me regret it. 🎂 `
+}
+
+];
